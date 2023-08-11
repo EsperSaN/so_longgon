967aa54ef309e0f44f69bce150abfa83f4b55773
Polawat Ruenruay <pruenrua@z3t1c1.42bangkok.com>
2023-08-11 15:46:35 +0700

    fix the norm and add the error when file is not valid

--- get_img_winsize.c
+++ get_img_winsize.c
@@ -6,16 +6,11 @@
 /*   By: pruenrua <pruenrua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 16:40:15 by pruenrua          #+#    #+#             */
-/*   Updated: 2023/08/11 11:59:27 by pruenrua         ###   ########.fr       */
+/*   Updated: 2023/08/11 15:46:11 by pruenrua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-//int		img_verify()
-//{
-
-//}
 
 void	get_window_size(char **map_array, int *width, int *hight, int img_size)
 {
@@ -23,8 +18,6 @@ void	get_window_size(char **map_array, int *width, int *hight, int img_size)
 
 	*width = 0;
 	*hight = 0;
-	if (!map_array)
-		return ((void)error(errno, "CANNOT GET THE WINSIZE SRY"));
 	*width = ft_strlen(map_array[0]);
 	while (map_array[*hight])
 		(*hight)++;
@@ -32,25 +25,50 @@ void	get_window_size(char **map_array, int *width, int *hight, int img_size)
 	*hight = (*hight) * img_size;
 }
 
-int	get_img_data(t_var *v)
+static int	get_xpm(t_var *v)
 {
 	t_img	*i;
+	int		j;
 
 	i = &v->img[FLOOR];
 	i->ref = mlx_xpm_file_to_image(v->mlx, FLOOR_P, &i->size.x, &i->size.y);
-	i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
 	i = &v->img[WALL];
 	i->ref = mlx_xpm_file_to_image(v->mlx, WALL_P, &i->size.x, &i->size.y);
-	i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
 	i = &v->img[PLAYER];
 	i->ref = mlx_xpm_file_to_image(v->mlx, PLAYER_P, &i->size.x, &i->size.y);
-	i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
 	i = &v->img[COL];
 	i->ref = mlx_xpm_file_to_image(v->mlx, COL_P, &i->size.x, &i->size.y);
-	i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
 	i = &v->img[EXIT];
 	i->ref = mlx_xpm_file_to_image(v->mlx, EXIT_P, &i->size.x, &i->size.y);
-	i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
-	
+	j = -1;
+	while (++j < 5)
+	{
+		if (v->img[j].ref == NULL || v->img[j].size.x != IMG_SIZE 
+			|| v->img[j].size.x != IMG_SIZE)
+			return (0);
+	}
 	return (1);
+}
+
+int	get_img_data(t_var *v)
+{
+	t_img	*i;
+	int		j;
+
+	if (get_xpm(v))
+	{
+		i = &v->img[FLOOR];
+		i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
+		i = &v->img[WALL];
+		i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
+		i = &v->img[PLAYER];
+		i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
+		i = &v->img[COL];
+		i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
+		i = &v->img[EXIT];
+		i->px = mlx_get_data_addr(i->ref, &i->bit_px, &i->line_size, &i->ed);
+		v->imgsize = v->img[0].size.x;
+		return (1);
+	}
+	return (0);
 }
